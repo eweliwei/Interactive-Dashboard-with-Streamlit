fd6fa461df22c8833191891fb86503e55e6429de
Li Weiiii <eweliwei1d5@gmail.com>
2023-05-29 08:39:55 +0800

    Initialize File

---
+++ dashboard.py
@@ -0,0 +1,9 @@
+# Developed using virtual environment
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+
+# To set a webpage title, header and subtitle
+st.set_page_config(page_title="Movies analysis", layout='wide')
